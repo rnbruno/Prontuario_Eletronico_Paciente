00ba30a3f8f6fdecb4c6202db394af3c5e8d9c75
root <root@DESKTOP-L40QG78>
2023-10-02 06:15:21 -0300

    atualizacao 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    EmployeeListComponent,
   ],
   imports: [
     BrowserModule,
