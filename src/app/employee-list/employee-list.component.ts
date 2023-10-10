45b2841b2a3a92354517827144c48ec84ce16600
root <root@DESKTOP-L40QG78>
2023-10-09 23:53:59 -0300

    atualizacao

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit {
+
+  employees: Employee[] = [];
+ 
+  constructor(private employeeService: EmployeeService){
+
+  }
+  ngOnInit(): void{
+
+    this.getEmployees();
+    // this.employees =[{
+    //   "id":1,
+    //   "firstName":"Ramesh",
+    //   "lastName": "Sulian",
+    //   "emailId": "ramesh@gmail.com"
+    // },{
+    //   "id":2,
+    //   "firstName":"John",
+    //   "lastName": "Fai",
+    //   "emailId": "johnf@gmail.com"
+    // }]
+  }
+  private getEmployees(){
+    this.employeeService.getEmployeesList().subscribe(data =>{
+      console.log(data);
+      this.employees = data;
+    });
+  }
 
 }
